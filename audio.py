0308f0464d0aea11d4dc35218301645eb8943a03
Kotaro Kashihara <kotaro11kassy@gmail.com>
2024-06-15 10:56:03 +0900

    feat: add audio analysis

--- audio.py
+++ audio.py
@@ -1,24 +1,41 @@
-import librosa
 import numpy as np
+import opensmile
+
 
 # 読み込む音声ファイル
-audio_file = (
-    "./data/preprocessed_data/voice/riko/1/audioNLPTarouTokushim11736812730.mp3"
+audio_file = "./test.wav"
+
+# # librosaでのピッチ計算
+# y, sr = librosa.load(audio_file, sr=None)
+# f0, voiced_flag, voiced_probs = librosa.pyin(
+#     y, fmin=librosa.note_to_hz("C2"), fmax=librosa.note_to_hz("C7")
+# )
+# f0 = f0[~np.isnan(f0)]
+# pitch_mean = f0.mean()
+# print("Average Pitch:", pitch_mean)
+
+# # Librosaでのスピーチレートの計算
+# frames = librosa.util.frame(y, frame_length=512, hop_length=64)
+# speech_rate = np.sum(frames != 0) / (len(y) / sr)
+# print(f"Speech rate: {speech_rate:.2f} words per second")
+
+
+# OpenSMILEでのピッチ計算
+smile = opensmile.Smile(
+    feature_set=opensmile.FeatureSet.eGeMAPSv02,
+    feature_level=opensmile.FeatureLevel.Functionals,
 )
+features = smile.process_file(audio_file)
+pitch_values = features["F0semitoneFrom27.5Hz_sma3nz_amean"]
+print("Average Pitch:", pitch_values.mean())
 
-# 音声ファイルを読み込む
-y, sr = librosa.load(audio_file, sr=None)
 
-# tempo, _ = librosa.beat.beat_track(y)
-# print(tempo)
+# OpenSMILEでのジッター計算
+loudness = features["loudness_sma3_amean"]
+print(loudness)
+mean_loudness = np.mean(loudness)
+print(f"Mean Loudness: {mean_loudness}")
 
-# 基本周波数 (F0) を計算する
-f0, voiced_flag, voiced_probs = librosa.pyin(
-    y, fmin=librosa.note_to_hz("C2"), fmax=librosa.note_to_hz("C7")
-)
-f0 = f0[~np.isnan(f0)]
-
-# 基本周波数の平均値を計算
-pitch_mean = f0.mean()
-
-print("Average Pitch:", pitch_mean)
+jitters = features["jitterLocal_sma3nz_amean"]
+mean_jitter = np.mean(jitters)
+print(f"Mean Jitter: {mean_jitter}")
