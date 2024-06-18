3429d37c41f6efa0731130d17f2c6797242aca72
Kotaro Kashihara <kotaro11kassy@gmail.com>
2024-06-18 14:20:56 +0900

    feat: percentage of p/n words

--- feature_extraction/text.py
+++ feature_extraction/text.py
@@ -13,45 +13,89 @@ column_names = {
     "PositiveVerbAdjCount": "P_VerbAdj",
     "NegativeWordCount": "N_Word",
     "PositiveWordCount": "P_Word",
+    "PercentageNegativeNouns": "Per_Noun_Neg",
+    "PercentagePositiveNouns": "Per_Noun_Pos",
+    "PercentageNegativeVerbAdj": "Per_VerbAdj_Neg",
+    "PercentagePositiveVerbAdj": "Per_VerbAdj_Pos",
 }
 
 
 def count_negative_words(texts, negative_nouns, negative_verb_adj):
     negative_noun_count = 0
+    total_noun_count = 0
     negative_verb_count = 0
+    total_verb_adj_count = 0
     for text in texts:
         doc = nlp(text)
         for token in doc:
             # ネガティブな名詞をカウント
-            if token.pos_ == "NOUN" and token.lemma_ in negative_nouns:
-                logger.info(f"Negative noun：{token.lemma_}")
-                negative_noun_count += 1
+            if token.pos_ == "NOUN":
+                total_noun_count += 1
+                if token.lemma_ in negative_nouns:
+                    logger.info(f"Negative noun：{token.lemma_}")
+                    negative_noun_count += 1
             # ネガティブな用言をカウント
-            elif token.pos_ in ["VERB", "ADJ"] and token.lemma_ in negative_verb_adj:
-                logger.info(f"Negative verb or adj：{token.lemma_}")
-                negative_verb_count += 1
+            elif token.pos_ in ["VERB", "ADJ"]:
+                total_verb_adj_count += 1
+                if token.lemma_ in negative_verb_adj:
+                    logger.info(f"Negative verb or adj：{token.lemma_}")
+                    negative_verb_count += 1
+
+    percentage_negative_nouns = negative_noun_count / total_noun_count
+    percentage_negative_verb_adj = negative_verb_count / total_verb_adj_count
     logger.info(f"Negative noun count: {negative_noun_count}")
+    logger.info(
+        f"Percentage of negative nouns among all nouns: {percentage_negative_nouns}"
+    )
     logger.info(f"Negative verb and adj count: {negative_verb_count}")
-    return negative_noun_count, negative_verb_count
+    logger.info(
+        f"Percentage of negative verb and adj among all verb and adj: {percentage_negative_verb_adj}"
+    )
+    return (
+        negative_noun_count,
+        negative_verb_count,
+        percentage_negative_nouns,
+        percentage_negative_verb_adj,
+    )
 
 
 def count_positive_words(texts, positive_nouns, positive_verb_adj):
     positive_noun_count = 0
+    total_noun_count = 0
     positive_verb_count = 0
+    total_verb_adj_count = 0
     for text in texts:
         doc = nlp(text)
         for token in doc:
             # ポジティブな名詞をカウント
-            if token.pos_ == "NOUN" and token.lemma_ in positive_nouns:
-                logger.info(f"Positive noun：{token.lemma_}")
-                positive_noun_count += 1
+            if token.pos_ == "NOUN":
+                total_noun_count += 1
+                if token.lemma_ in positive_nouns:
+                    logger.info(f"Positive noun：{token.lemma_}")
+                    positive_noun_count += 1
             # ポジティブな用言をカウント
-            elif token.pos_ in ["VERB", "ADJ"] and token.lemma_ in positive_verb_adj:
-                logger.info(f"Positive verb or adj：{token.lemma_}")
-                positive_verb_count += 1
+            elif token.pos_ in ["VERB", "ADJ"]:
+                total_verb_adj_count += 1
+                if token.lemma_ in positive_verb_adj:
+                    logger.info(f"Positive verb or adj：{token.lemma_}")
+                    positive_verb_count += 1
+
+    percentage_positive_nouns = positive_noun_count / total_noun_count
+    percentage_positive_verb_adj = positive_verb_count / total_verb_adj_count
     logger.info(f"Positive noun count: {positive_noun_count}")
+    logger.info(
+        f"Percentage of positive nouns among all nouns: {percentage_positive_nouns}"
+    )
     logger.info(f"Positive verb and adj count: {positive_verb_count}")
-    return positive_noun_count, positive_verb_count
+    logger.info(
+        f"Percentage of positive verb and adj among all verb and adj: {percentage_positive_verb_adj}"
+    )
+    return (
+        positive_noun_count,
+        positive_verb_count,
+        percentage_positive_nouns,
+        percentage_positive_verb_adj,
+    )
 
 
 def get_negative_nouns(nouns_file):
@@ -94,6 +138,59 @@ def get_positive_verb_adj(verbs_file):
     return positive_verbs
 
 
+def add_result(
+    before_sum_df,
+    target,
+    negative_noun_count,
+    negative_verb_adj_count,
+    positive_nouns_count,
+    positive_verb_adj_count,
+    percentage_negative_nouns,
+    percentage_negative_verb_adj,
+    percentage_positive_nouns,
+    percentage_positive_verb_adj,
+):
+    before_sum_df.loc[
+        before_sum_df["タイムスタンプ"] == target, column_names["NegativeNounCount"]
+    ] = negative_noun_count
+    before_sum_df.loc[
+        before_sum_df["タイムスタンプ"] == target,
+        column_names["NegativeVerbAdjCount"],
+    ] = negative_verb_adj_count
+    before_sum_df.loc[
+        before_sum_df["タイムスタンプ"] == target, column_names["NegativeWordCount"]
+    ] = negative_noun_count + negative_verb_adj_count
+
+    before_sum_df.loc[
+        before_sum_df["タイムスタンプ"] == target, column_names["PositiveNounCount"]
+    ] = positive_nouns_count
+    before_sum_df.loc[
+        before_sum_df["タイムスタンプ"] == target,
+        column_names["PositiveVerbAdjCount"],
+    ] = positive_verb_adj_count
+    before_sum_df.loc[
+        before_sum_df["タイムスタンプ"] == target, column_names["PositiveWordCount"]
+    ] = positive_nouns_count + positive_verb_adj_count
+
+    before_sum_df.loc[
+        before_sum_df["タイムスタンプ"] == target,
+        column_names["PercentagePositiveNouns"],
+    ] = percentage_positive_nouns
+    before_sum_df.loc[
+        before_sum_df["タイムスタンプ"] == target,
+        column_names["PercentagePositiveVerbAdj"],
+    ] = percentage_positive_verb_adj
+    before_sum_df.loc[
+        before_sum_df["タイムスタンプ"] == target,
+        column_names["PercentageNegativeNouns"],
+    ] = percentage_negative_nouns
+    before_sum_df.loc[
+        before_sum_df["タイムスタンプ"] == target,
+        column_names["PercentageNegativeVerbAdj"],
+    ] = percentage_negative_verb_adj
+    return before_sum_df
+
+
 def analyze_ginza(before_sum_df):
     riko_text_files = glob.glob(
         os.path.join("../data/preprocessed_data/text/riko", "*", "*.csv"),
@@ -119,34 +216,31 @@ def analyze_ginza(before_sum_df):
             target = f"riko0{id}"
         else:
             target = f"riko{id}"
-        negative_noun_count, negative_verb_adj_count = count_negative_words(
-            texts, negative_nouns, negative_verb_adj
-        )
-        positive_nouns_count, positive_verb_adj_count = count_positive_words(
-            texts, positive_nouns, positive_verb_adj
-        )
+        (
+            negative_noun_count,
+            negative_verb_adj_count,
+            percentage_negative_nouns,
+            percentage_negative_verb_adj,
+        ) = count_negative_words(texts, negative_nouns, negative_verb_adj)
+        (
+            positive_nouns_count,
+            positive_verb_adj_count,
+            percentage_positive_nouns,
+            percentage_positive_verb_adj,
+        ) = count_positive_words(texts, positive_nouns, positive_verb_adj)
 
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target, column_names["NegativeNounCount"]
-        ] = negative_noun_count
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target,
-            column_names["NegativeVerbAdjCount"],
-        ] = negative_verb_adj_count
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target, column_names["NegativeWordCount"]
-        ] = negative_noun_count + negative_verb_adj_count
-
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target, column_names["PositiveNounCount"]
-        ] = positive_nouns_count
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target,
-            column_names["PositiveVerbAdjCount"],
-        ] = positive_verb_adj_count
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target, column_names["PositiveWordCount"]
-        ] = positive_nouns_count + positive_verb_adj_count
+        before_sum_df = add_result(
+            before_sum_df,
+            target,
+            negative_noun_count,
+            negative_verb_adj_count,
+            positive_nouns_count,
+            positive_verb_adj_count,
+            percentage_negative_nouns,
+            percentage_negative_verb_adj,
+            percentage_positive_nouns,
+            percentage_positive_verb_adj,
+        )
 
     for igaku_text_file in igaku_text_files:
         logger.info(f"Counting negative words from {igaku_text_file}....")
@@ -154,33 +248,29 @@ def analyze_ginza(before_sum_df):
         texts = igaku_texts.iloc[:, 2].tolist()
         id = igaku_text_file.split("/")[-2]
         target = f"psy_c_{id}"
-        negative_noun_count, negative_verb_adj_count = count_negative_words(
-            texts, negative_nouns, negative_verb_adj
+        (
+            negative_noun_count,
+            negative_verb_adj_count,
+            percentage_negative_nouns,
+            percentage_negative_verb_adj,
+        ) = count_negative_words(texts, negative_nouns, negative_verb_adj)
+        (
+            positive_nouns_count,
+            positive_verb_adj_count,
+            percentage_positive_nouns,
+            percentage_positive_verb_adj,
+        ) = count_positive_words(texts, positive_nouns, positive_verb_adj)
+
+        before_sum_df = add_result(
+            before_sum_df,
+            target,
+            negative_noun_count,
+            negative_verb_adj_count,
+            positive_nouns_count,
+            positive_verb_adj_count,
+            percentage_negative_nouns,
+            percentage_negative_verb_adj,
+            percentage_positive_nouns,
+            percentage_positive_verb_adj,
         )
-        positive_nouns_count, positive_verb_adj_count = count_positive_words(
-            texts, positive_nouns, positive_verb_adj
-        )
-
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target, column_names["NegativeNounCount"]
-        ] = negative_noun_count
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target,
-            column_names["NegativeVerbAdjCount"],
-        ] = negative_verb_adj_count
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target, column_names["NegativeWordCount"]
-        ] = negative_noun_count + negative_verb_adj_count
-
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target, column_names["PositiveNounCount"]
-        ] = positive_nouns_count
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target,
-            column_names["PositiveVerbAdjCount"],
-        ] = positive_verb_adj_count
-        before_sum_df.loc[
-            before_sum_df["タイムスタンプ"] == target, column_names["PositiveWordCount"]
-        ] = positive_nouns_count + positive_verb_adj_count
-
     return before_sum_df
